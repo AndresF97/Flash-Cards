20911c0d1bf2fbcd0b0ee5116b92a5000f466841
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-04-27 22:40:05 -0700

    started on basci structure for flash card

--- flash-cards/src/components/howTo/index.js
+++ flash-cards/src/components/howTo/index.js
@@ -1,15 +1,26 @@
 import React from "react"
-import {Container} from "react-bootstrap"
+import {Container,Row,Col} from "react-bootstrap"
 import "./style.css"
 
 
 function howTo(){
     return(
     <>
-        <Container>
+        <Container className="text-center">
+            <Row>
+            <Col>
+            <h2>Front</h2>
             <div className="flashCard">
                 heleleodjsadjksaljkaj
             </div>
+            </Col>
+            <Col>
+            <h2>Back</h2>
+            <div className="flashCard">
+                heleleodjsadjksaljkaj
+            </div>
+            </Col>
+            </Row>
             </Container>
          </>
     )
