6a84bb1fccda3ab72d155e82fc0c6a09d6d63cb2
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-04-28 22:38:19 -0700

    added an other lay outfor a card

--- flash-cards/src/components/howTo/index.js
+++ flash-cards/src/components/howTo/index.js
@@ -12,14 +12,30 @@ function howTo(){
             <h2>Front</h2>
             <div className="flashCard">
                 heleleodjsadjksaljkaj
+                <hr style={{borderTop:"1px solid red"}}></hr>
+                
             </div>
             </Col>
             <Col>
             <h2>Back</h2>
             <div className="flashCard">
                 heleleodjsadjksaljkaj
+                
             </div>
             </Col>
+            </Row>
+            <h2>Or you can use this design:</h2>
+            <Row className="text-center">
+                <Col>
+                <h3>Front and Back</h3>           
+                <div className="flashCard">
+                heleleodjsadjksaljkaj
+                <hr style={{borderTop:"1px solid red"}}></hr>
+                <p>
+
+                </p>
+                </div>
+                </Col>
             </Row>
             </Container>
          </>
